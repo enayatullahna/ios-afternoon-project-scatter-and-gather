fdcfaf4cf06c40cce07c084b3ab14757e00d4015
Enayatullah Naseri <ena.nas@icloud.com>
2019-06-25 19:38:43 -0700

    Working on the project searcing sources

--- ScatterandGatherAnimations/ScatterandGatherAnimations/ViewController.swift
+++ ScatterandGatherAnimations/ScatterandGatherAnimations/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var isScattered: Bool = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var lLabel: UILabel!
+    @IBOutlet weak var aLabel: UILabel!
+    @IBOutlet weak var mLabel: UILabel!
+    @IBOutlet weak var bLabel: UILabel!
+    @IBOutlet weak var dLabel: UILabel!
+    @IBOutlet weak var a2Label: UILabel!
+    
+    
+    
+    
+    @IBAction func toggleButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
 }
 
